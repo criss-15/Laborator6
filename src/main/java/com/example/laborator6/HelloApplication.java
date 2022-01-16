efd2f737c909eac3a4233f2fb2c0fdc37c611927
Oros Cristina <oros.cristina15@gmail.com>
2022-01-16 12:43:12 +0200

    last commit

--- src/main/java/com/example/laborator6/HelloApplication.java
+++ src/main/java/com/example/laborator6/HelloApplication.java
@@ -4,47 +4,7 @@ import controller.RegistrationSystem;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import repository.CourseRepository;
-import repository.StudentRepository;
-import repository.TeacherRepository;
 
-
-
-//public class HelloApplication extends Application {
-//
-//    private RegistrationSystem registrationSystem;
-//    private StudentRepository studentRepository;
-//    private CourseRepository courseRepository;
-//    private TeacherRepository teacherRepository;
-//
-//
-//    public void input()
-//    {
-//        StudentRepository studentRepository = new StudentRepository("jdbc:mysql://localhost:3306/school", "root", "SimoInfo15");
-//        CourseRepository courseRepository = new CourseRepository("jdbc:mysql://localhost:3306/school", "root", "SimoInfo15");
-//        TeacherRepository teacherRepository = new TeacherRepository("jdbc:mysql://localhost:3306/school", "root", "SimoInfo15");
-//        registrationSystem = new RegistrationSystem("jdbc:mysql://localhost:3306/school", "root", "SimoInfo15");
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    public static void main(String[] args) {
-//        launch();
-//    }
-//
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//
-//    }
-//}
 
 
 
